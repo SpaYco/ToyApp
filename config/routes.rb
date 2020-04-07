8f0865749f8b22703438cab7b2a25747b3b9d327
tahirbhalli <tahirbhalli16@gmail.com>
2020-04-07 19:27:54 +0500

    scfold and migrations of db

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  
+  
+  resources :users
+  root 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  
 end
